f9f59d617535607225a09d917b9629e0f39a6a0b
Douglas Rohr <douglasRohr@users.noreply.github.com>
2023-12-21 17:20:11 -0300

    Ajuste na identação do menu e inserção do TRY/EXCEPT

--- Conversor_medidas.py
+++ Conversor_medidas.py
@@ -5,19 +5,30 @@
 # CÓDIGO QUEBRA QUANDO O USUÁRIO INSERE LETRAS
 
 while True:
-    menu = int(input('Bem-vindo ao conversor de medidas!\nO que deseja converter?\n[1] Metros em centímetros\n[2] Centímetros em metros\n[3] Sair\nDigite sua opção: '))
+    menu = int(input('''
+Bem-vindo ao conversor de medidas!
+
+O que deseja converter?
+                                       
+[1] Metros em centímetros
+[2] Centímetros em metros
+[3] Sair
+                                         
+Digite sua opção: '''))
+    
     if menu == 3:
         print('Até a próxima!')
         exit()
-    elif menu >= 1 or menu <= 2: # APÓS O CÁLCULO, O CÓDIGO RETORNA AO MENU INDESEJADAMENTE
-        match menu:
-            case 1:
-                calc_ctms = float(input('Digite o valor em metro(s) a converter: '))
-                print(f'Convertendo {calc_ctms} metro(s) para centímetro(s) obtemos: {calc_ctms * 100} centímetros\n')
-            case 2:
-                calc_mts = float(input('Valor em centímetro(s) a converter: '))
-                print(f'Convertendo {calc_mts} centímetro(s) para metro(s) obtemos: {calc_mts / 100}')
-            case _:
-                print('Opção inválida. Tente novamente.')
-    else:
+    try:
+        if menu >= 1 or menu <= 2: # APÓS O CÁLCULO, O CÓDIGO RETORNA AO MENU INDESEJADAMENTE
+            match menu:
+                case 1:
+                    calc_ctms = float(input('Digite o valor em metro(s) a converter: '))
+                    print(f'Convertendo {calc_ctms} metro(s) para centímetro(s) obtemos: {calc_ctms * 100} centímetros\n')
+                case 2:
+                    calc_mts = float(input('Valor em centímetro(s) a converter: '))
+                    print(f'Convertendo {calc_mts} centímetro(s) para metro(s) obtemos: {calc_mts / 100}')
+                case _:
+                    print('Opção inválida. Tente novamente.')
+    except:
         print('Opção inválida. Tente novamente.')
